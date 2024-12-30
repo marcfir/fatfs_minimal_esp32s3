68177a18dbe61437090402e59c8ba1cfc11352f1
marcfir <marcfir@proton.me>
2024-12-30 15:32:02 +0100

    Update to esp_hal:v0.22

--- src/main.rs
+++ src/main.rs
@@ -9,70 +9,66 @@ use embedded_hal_async::delay::DelayNs;
 use embedded_hal_bus::spi::ExclusiveDevice;
 use embedded_io_async::{Read, Seek, Write};
 use esp_backtrace as _;
-use esp_hal::gpio::Io;
+use esp_hal::dma::DmaRxBuf;
+use esp_hal::dma::DmaTxBuf;
+use esp_hal::dma_buffers;
 use esp_hal::gpio::Level;
 use esp_hal::gpio::Output;
-use esp_hal::system::SystemControl;
+use esp_hal::spi::master::Config;
+use esp_hal::spi::master::SpiDmaBus;
 use esp_hal::timer::timg::TimerGroup;
-use esp_hal::timer::ErasedTimer;
-use esp_hal::timer::OneShotTimer;
+
 use esp_hal::{
-    clock::ClockControl,
     dma::Dma,
     dma::DmaPriority,
-    dma_descriptors,
-    peripherals::Peripherals,
     prelude::*,
-    spi::{
-        master::{prelude::*, Spi},
-        SpiMode,
-    },
-    FlashSafeDma,
+    spi::{master::Spi, SpiMode},
 };
 use sdspi::SdSpi;
-use static_cell::StaticCell;
 
 #[main]
 async fn main(_spawner: Spawner) {
-    let peripherals = Peripherals::take();
-    let system = SystemControl::new(peripherals.SYSTEM);
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
-
-    static ONE_SHOT_TIMER: StaticCell<[OneShotTimer<ErasedTimer>; 1]> = StaticCell::new();
-    esp_hal_embassy::init(
-        &clocks,
-        ONE_SHOT_TIMER.init([OneShotTimer::new(timg0.timer0.into())]),
-    );
-
     esp_println::logger::init_logger_from_env();
     log::info!("Hello world!");
+    let peripherals = esp_hal::init(esp_hal::Config::default());
 
-    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let sclk = io.pins.gpio12;
-    let miso = io.pins.gpio11;
-    let mosi = io.pins.gpio13;
-    let mut cs = Output::new(io.pins.gpio10, Level::Low);
+    let timg0 = TimerGroup::new(peripherals.TIMG0);
+    esp_hal_embassy::init(timg0.timer0);
+
+    // let io = Io::new(peripherals.IO_MUX);
+    let sclk = peripherals.GPIO12;
+    let miso = peripherals.GPIO11;
+    let mosi = peripherals.GPIO13;
+    let mut cs = Output::new(peripherals.GPIO10, Level::High);
+
+    // let sclk = peripherals.GPIO12;
+    // let miso = peripherals.GPIO11;
+    // let mosi = peripherals.GPIO13;
+    // let cs = peripherals.GPIO10;
 
     let dma = Dma::new(peripherals.DMA);
     let dma_channel = dma.channel0;
 
-    let (descriptors, rx_descriptors) = dma_descriptors!(32000);
+    let (rx_buffer, rx_descriptors, tx_buffer, tx_descriptors) = dma_buffers!(32000);
+    let dma_rx_buf = DmaRxBuf::new(rx_descriptors, rx_buffer).unwrap();
+    let dma_tx_buf = DmaTxBuf::new(tx_descriptors, tx_buffer).unwrap();
 
-    // Initialize spi at the maxiumum SD initialization frequency of 400 khz
-    let spi = Spi::new(peripherals.SPI2, 400u32.kHz(), SpiMode::Mode0, &clocks)
-        .with_sck(sclk)
-        .with_miso(miso)
-        .with_mosi(mosi)
-        .with_dma(
-            dma_channel.configure_for_async(false, DmaPriority::Priority0),
-            descriptors,
-            rx_descriptors,
-        );
+    let spi = Spi::new_with_config(
+        peripherals.SPI2,
+        Config {
+            frequency: 400.kHz(),
+            mode: SpiMode::Mode0,
+            ..Config::default()
+        },
+    )
+    .with_sck(sclk)
+    .with_mosi(mosi)
+    .with_miso(miso)
+    // .with_cs(cs)
+    .with_dma(dma_channel.configure(false, DmaPriority::Priority0))
+    .into_async();
 
-    let mut spi = FlashSafeDma::<_, 512>::new(spi);
-    // Sd cards need to be clocked with a at least 74 cycles on their spi clock without the cs enabled,
-    // sd_init is a helper function that does this for us.
+    let mut spi = SpiDmaBus::new(spi, dma_rx_buf, dma_tx_buf);
     loop {
         match sdspi::sd_init(&mut spi, &mut cs).await {
             Ok(_) => break,
@@ -89,11 +85,15 @@ async fn main(_spawner: Spawner) {
     loop {
         // Initialize the card
         if (sd.init().await).is_ok() {
-            // Increase the speed up to the SD max of 25mhz
+            // Increase the speed up to the SD max of
             sd.spi()
                 .bus_mut()
-                .inner_mut()
-                .change_bus_frequency(25u32.MHz(), &clocks);
+                .apply_config(&Config {
+                    frequency: 25u32.MHz(),
+                    mode: SpiMode::Mode0,
+                    ..Config::default()
+                })
+                .unwrap();
             log::info!("Initialization complete!");
 
             break;
